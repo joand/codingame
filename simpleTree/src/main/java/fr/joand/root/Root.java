e44bfeda07f404c2f00863e0087ab819a920e6fc
Joel ANDRIAMANAMPISOA <joelandria@gmail.com>
2017-08-15 18:48:51 +0200

    Tree !

--- simpleTree/src/main/java/fr/joand/root/Root.java
+++ simpleTree/src/main/java/fr/joand/root/Root.java
@@ -1,23 +1,32 @@
+/*******
+ * Read input from System.in
+ * Use System.out.println to ouput your result.
+ * Use:
+ *  IsoContestBase.localEcho( variable)
+ * to display variable in a dedicated area.
+ * ***/
 package fr.joand.root;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Root {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        String keyA = "A";
-        String keyB = "B";
-
+        Scanner sc = new Scanner(System.in);
+        int nbOfRows = Integer.valueOf(sc.nextLine());
+        //IsoContestBase.localEcho("nbOfRows : "+nbOfRows);
         Map<String, Node> allNodes = new HashMap<>();
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            String[] splitedLine = line.split(" ");
+            String keyA = splitedLine[0];
+            String keyB = splitedLine[1];
 
-        Node nodeA = getNodeOrStoreIt(keyA, allNodes);
-        Node nodeB = getNodeOrStoreIt(keyB, allNodes);
+            Node nodeA = getNodeOrStoreIt(keyA, allNodes);
+            Node nodeB = getNodeOrStoreIt(keyB, allNodes);
 
-        nodeA.addChild(nodeB);
+            nodeA.addChild(nodeB);
+        }
 
         System.out.println(getMaxHeight(new ArrayList(allNodes.values())));
     }
@@ -49,3 +58,70 @@ public class Root {
         }
     }
 }
+
+class Node {
+    final String data;
+    Node parent;
+    List<Node> children = new ArrayList<>();
+
+    /**
+     * https://stackoverflow.com/questions/2603692/what-is-the-difference-between-tree-depth-and-height
+     */
+    int height = 1;
+
+    public Node(String data) {
+        this.data = data;
+    }
+
+    public void addChild(Node child) {
+        this.children.add(child);
+        computeHeight();
+
+        child.setParent(this);
+    }
+
+    void computeHeight() {
+        this.height = 1 + Root.getMaxHeight(this.children);
+        if (this.parent != null) {
+            this.parent.computeHeight();
+        }
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    public List<Node> getChildren() {
+        return children;
+    }
+
+    public boolean isLeaf() {
+        return children.isEmpty();
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node node = (Node) o;
+        return Objects.equals(data, node.data);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(data);
+    }
+}
+
