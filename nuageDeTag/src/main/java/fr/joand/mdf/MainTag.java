a6dc1bd1b556f71fdefe4b3ec82428c7fab34ec2
Joel ANDRIAMANAMPISOA <joelandria@gmail.com>
2017-08-22 22:19:56 +0200

    nuage de tag MDF 2015

---
+++ nuageDeTag/src/main/java/fr/joand/mdf/MainTag.java
@@ -0,0 +1,67 @@
+package fr.joand.mdf;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class MainTag {
+    public static void main(String[] argv) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        int nbTotalTag = Integer.parseInt(sc.nextLine());
+
+        List<Tag> allTags = new ArrayList<>();
+        for (int nbTag = 0; nbTag < nbTotalTag; nbTag++) {
+            String tagName = sc.nextLine();
+            Tag tag = new Tag(tagName);
+            if (allTags.contains(tag)) {
+                int index = allTags.indexOf(tag);
+                tag = allTags.get(index);
+                tag.increment();
+            }
+            /* Lisez les données et effectuez votre traitement */
+        }
+        final StringBuilder sb = new StringBuilder();
+        allTags.stream()
+                .sorted((o1, o2) -> o2.getOccurence() - o1.getOccurence())
+                .limit(5)
+                .forEach(tag -> sb.append(tag.getName() + " " + tag.getOccurence()+"\n"));
+        System.out.println(sb.toString());
+    /* Vous pouvez aussi effectuer votre traitement une fois que vous avez lu toutes les données.*/
+    }
+}
+
+class Tag {
+    final String name;
+    int occurence = 1;
+
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    void increment() {
+        occurence++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getOccurence() {
+        return occurence;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tag tag = (Tag) o;
+        return Objects.equals(name, tag.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
